f233404c7bc8ac01b45d998c406ffd3d10fa248e
GuillemPT <guillempt16@gmail.com>
2024-10-09 21:53:40 +0200

    feat(formatMethods): add reconigtion for other responses status

--- src/fileslogic/formatMethods.go
+++ src/fileslogic/formatMethods.go
@@ -5,10 +5,15 @@ import (
 	"strings"
 )
 
-const regexMinLength = 4
+const (
+	regexHeaderLength     = 4
+	regexStatusCodeLength = 2
+)
 
 var regex = regexp.MustCompile(
 	`\w+\.(get|post|put|delete)\((["'])([^"']+)(["'])`)
+
+var statusRegex = regexp.MustCompile(`res\.status\((\d{3})\)`)
 
 func FormatMethods(methods []string) map[string]pathDocument {
 	pathDocMap := make(map[string]pathDocument, len(methods))
@@ -33,11 +38,7 @@ func formatMethod(method string) formatResult {
 	var result formatResult
 	lines := strings.Split(method, "\n")
 	pathDoc := make(map[string]operationDocument)
-	optDoc := operationDocument{
-		Responses: map[string]responsesDocument{
-			"200": {Description: "Ok"},
-		},
-	}
+	optDoc := operationDocument{Responses: make(map[string]responsesDocument)}
 	inDescription := false
 
 	for _, line := range lines {
@@ -53,13 +54,28 @@ func formatMethod(method string) formatResult {
 		}
 		if !inDescription {
 			match := regex.FindStringSubmatch(line)
-			if len(match) >= regexMinLength {
+			if len(match) >= regexHeaderLength {
 				result.pathName = match[3]
 				result.operationName = match[1]
 				pathDoc[result.operationName] = optDoc
 			}
+
+			statusMatch := statusRegex.FindStringSubmatch(line)
+			if len(statusMatch) == regexStatusCodeLength {
+				statusCode := statusMatch[1]
+				optDoc.Responses[statusCode] = responsesDocument{
+					Description: "Response status: " + statusCode,
+				}
+			}
 		}
 	}
+	if len(optDoc.Responses) == 0 {
+		optDoc.Responses["200"] = responsesDocument{
+			Description: "Ok",
+		}
+	}
+
+	pathDoc[result.operationName] = optDoc
 	result.pathDoc = pathDoc
 	return result
 }
